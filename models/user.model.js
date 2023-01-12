aa79a37f861c1cba8bfd95f6759c5e2039f12429
Min Han <minhanchoo13@gmail.com>
2023-01-12 19:09:53 +0800

    store salt in users table

--- models/user.model.js
+++ models/user.model.js
@@ -36,6 +36,14 @@ const User = db.define('User', {
             isEmail: true,
         },
     },
+    salt: {
+        type: DataTypes.CHAR(60),
+        default: null,
+        validate: {
+            notEmpty: true,
+            len: [5, 25],
+        },
+    },
     password: {
         type: DataTypes.STRING(60),
         allowNull: false,
@@ -55,7 +63,8 @@ const User = db.define('User', {
 });
 
 User.beforeCreate(async (user) => {
-    user.password = await bcrypt.hash(user.password, 8);
+    user.salt = await bcrypt.genSalt(8);
+    user.password = await bcrypt.hash(user.password, user.salt);
 });
 
 User.prototype.validatePassword = function comparePassword(password) {
